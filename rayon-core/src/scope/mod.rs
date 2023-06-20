65574c37664fce8d2ab0c1ac7d4d346acb2027e4
Josh Stone <cuviper@gmail.com>
2023-06-20 13:53:19 -0700

    Improve inlining of scope latch counters
    The `increment` and `set` methods now have `#[inline]` hints, and the
    `counter` fields in `CountLatch` and `CountLockLatch` are now listed
    first to increase the chance that layout puts them at the same offset.
    (That layout is not critical to ensure, but works out nicely.)
    
    (cherry picked from commit f98eb5754aef15617bca78eb9d025ddccb00a70f)

--- rayon-core/src/scope/mod.rs
+++ rayon-core/src/scope/mod.rs
@@ -660,6 +660,7 @@ impl<'scope> ScopeBase<'scope> {
         }
     }
 
+    #[inline]
     fn increment(&self) {
         self.job_completed_latch.increment();
     }
@@ -719,17 +720,15 @@ impl<'scope> ScopeBase<'scope> {
     where
         FUNC: FnOnce() -> R,
     {
-        match unwind::halt_unwinding(func) {
-            Ok(r) => {
-                Latch::set(&(*this).job_completed_latch);
-                Some(r)
-            }
+        let result = match unwind::halt_unwinding(func) {
+            Ok(r) => Some(r),
             Err(err) => {
                 (*this).job_panicked(err);
-                Latch::set(&(*this).job_completed_latch);
                 None
             }
-        }
+        };
+        Latch::set(&(*this).job_completed_latch);
+        result
     }
 
     fn job_panicked(&self, err: Box<dyn Any + Send + 'static>) {
@@ -785,6 +784,7 @@ impl ScopeLatch {
         }
     }
 
+    #[inline]
     fn increment(&self) {
         match self {
             ScopeLatch::Stealing { latch, .. } => latch.increment(),
@@ -810,6 +810,7 @@ impl ScopeLatch {
 }
 
 impl Latch for ScopeLatch {
+    #[inline]
     unsafe fn set(this: *const Self) {
         match &*this {
             ScopeLatch::Stealing {
