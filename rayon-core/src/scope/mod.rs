2a25b537d03311693ba5e51d24c68e7e640d5d3a
Josh Stone <cuviper@gmail.com>
2024-03-23 21:00:13 -0700

    Allow clippy::type_complexity
    (cherry picked from commit a14f4591478c1ac73fd0a07492870d6898f6552d)

--- rayon-core/src/scope/mod.rs
+++ rayon-core/src/scope/mod.rs
@@ -56,6 +56,7 @@ struct ScopeBase<'scope> {
     /// all of which outlive `'scope`.  They're not actually required to be
     /// `Sync`, but it's still safe to let the `Scope` implement `Sync` because
     /// the closures are only *moved* across threads to be executed.
+    #[allow(clippy::type_complexity)]
     marker: PhantomData<Box<dyn FnOnce(&Scope<'scope>) + Send + Sync + 'scope>>,
 
     /// The TLV at the scope's creation. Used to set the TLV for spawned jobs.
